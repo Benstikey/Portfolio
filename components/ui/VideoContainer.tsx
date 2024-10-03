bd455a728d564e9db4a2162640ffd7574c620595
Benstikey <90990550+Benstikey@users.noreply.github.com>
2024-10-03 19:00:24 +0100

    1.0.15
    Minor adjustments to the videos section

--- components/ui/VideoContainer.tsx
+++ components/ui/VideoContainer.tsx
@@ -22,10 +22,11 @@ const VideoContainer: React.FC<VideoContainerProps> = ({
 }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [isMobile, setIsMobile] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const checkMobile = () => {
-      setIsMobile(window.innerWidth <= 768); // Adjust this value as needed
+      setIsMobile(window.innerWidth <= 768);
     };
     checkMobile();
     window.addEventListener('resize', checkMobile);
@@ -38,50 +39,65 @@ const VideoContainer: React.FC<VideoContainerProps> = ({
       video.load();
       const playPromise = video.play();
       if (playPromise !== undefined) {
-        playPromise.catch(error => {
-          console.error("Autoplay prevented:", error);
-          // Set poster image as fallback
-          video.poster = `${videoSrc}#t=0.001`;
-        });
+        playPromise
+          .then(() => {
+            setIsLoading(false);
+          })
+          .catch(error => {
+            console.error("Autoplay prevented:", error);
+            video.poster = `${videoSrc}#t=0.001`;
+            setIsLoading(false);
+          });
       }
     }
   }, [videoSrc]);
 
   const marginClasses = `${mobileMarginTop} ${mobileMarginBottom} ${desktopMarginTop} ${desktopMarginBottom}`;
 
+  const Skeleton = () => (
+    <div className="absolute inset-0 bg-gray-200 overflow-hidden">
+      <div className="wave-skeleton"></div>
+    </div>
+  );
+
   return (
     <div className={`relative w-full rounded-xl overflow-hidden shadow-custom ${marginClasses} ${className} group`}>
-      {isMobile ? (
-        <Image
-          src={`${videoSrc}#t=0.001`}
-          alt="Video Poster"
-          layout="fill"
-          objectFit="cover"
-        />
-      ) : (
-        <video
-          ref={videoRef}
-          className="w-full h-full object-cover transition-all duration-300 md:group-hover:blur-sm"
-          loop
-          muted
-          playsInline
-          preload="none"
-          poster={`${videoSrc}#t=0.001`}
+      <div className="relative w-full pt-[125%]">
+        {isLoading && <Skeleton />}
+        {isMobile ? (
+          <Image
+            src={`${videoSrc}#t=0.001`}
+            alt="Video Poster"
+            layout="fill"
+            objectFit="cover"
+            onLoadingComplete={() => setIsLoading(false)}
+          />
+        ) : (
+          <video
+            ref={videoRef}
+            className="absolute top-0 left-0 w-full h-full object-cover transition-all duration-300 md:group-hover:blur-sm"
+            loop
+            muted
+            playsInline
+            preload="none"
+            poster={`${videoSrc}#t=0.001`}
+            onLoadedData={() => setIsLoading(false)}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        )}
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
+          <h2 className="text-white font-bold text-xl">Visit the website</h2>
+        </div>
+        <a
+          href={linkHref}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute inset-0 z-10"
         >
-          <source src={videoSrc} type="video/mp4" />
-        </video>
-      )}
-      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 md:group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
-        <h2 className="text-white font-bold text-xl">Visit the website</h2>
+          <span className="sr-only">Visit website</span>
+        </a>
       </div>
-      <a
-        href={linkHref}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="absolute inset-0 z-10"
-      >
-        <span className="sr-only">Visit website</span>
-      </a>
     </div>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, { isServer }) {
-      // Add SVG support
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      // Important to handle server-side rendering (SSR) properly
-      if (!isServer) {
-        config.resolve.fallback.fs = false;
-      }
-  
-      return config;
-    },
-  };
-  
+  images: {
+    domains: ['wasben.com'],
+  },
+  webpack(config, { isServer }) {
+    // Add SVG support
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    // Important to handle server-side rendering (SSR) properly
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
+};
+
 export default nextConfig;
